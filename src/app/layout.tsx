f6683247faa5743cd13a1dc8881a4dfc1875277d
Garongan <alvindo56@gmail.com>
2025-01-13 23:12:38 +0700

    docs: ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,36 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {ThemeProvider} from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 import CustomNavbar from "@/components/custom-navbar";
 import Wave from "@/components/wave";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "User Registration Tournament",
-    description: "Created with ❤️ by @alvindotri.j",
+  title: "User Registration Tournament",
+  description: "Created with ❤️ by @alvindotri.j",
 };
 
-export default function RootLayout(
-    {children,}: Readonly<{ children: React.ReactNode; }>
-) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <main className="relative min-h-screen">
-                <div className="relative z-50 container mx-auto p-4 sm:p-8">
-                    <CustomNavbar/>
-                    <div className="py-10">
-                        {children}
-                    </div>
-                </div>
-                <div className="overflow-hidden w-full h-full z-10 absolute bottom-0 left-0">
-                    <Wave/>
-                </div>
-            </main>
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <main className='relative min-h-screen'>
+            <div className='relative z-50 container mx-auto p-4 sm:p-8'>
+              <CustomNavbar />
+              <div className='py-10'>{children}</div>
+            </div>
+            <div className='overflow-hidden w-full h-full z-10 absolute bottom-0 left-0'>
+              <Wave />
+            </div>
+          </main>
         </ThemeProvider>
-        </body>
-        </html>
-    )
-        ;
+      </body>
+    </html>
+  );
 }
